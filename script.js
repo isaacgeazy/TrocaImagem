288dbd1484f158fbd243404870c323c6f5007c04
Isaac Rocha <isaacgeazy@gmail.com>
2021-10-31 12:25:06 -0300

    finalizado

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function mudarImagem() {
     var text = window.document.getElementById('texto');
     text.style.fontSize = '30px';
     text.innerText = "ESCURIDÃO";
+    text.style.transition = "all 1.7s";
     var imagem = window.document.getElementById('imagem');
     imagem.src = "img/nocturn.jpg";
     var cor = window.document.getElementById('container').style.backgroundColor = 'rgb(70, 70, 70)';
-
 
 }
